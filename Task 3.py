c829c19a733fe32eb20b3546522de26a0156e716
NehaSN <88273493+NehaSN@users.noreply.github.com>
2023-01-02 08:44:54 +0530

    Set Operations

---
+++ Task 3.py
@@ -0,0 +1,15 @@
+# define two sets
+E = {6, 7, 8, 0, 9};
+N = {1, 3, 5, 4, 0};
+
+#union
+print("Union of E and N is",E | N)
+
+#intersection
+print("Intersection of E and N is", E & N)
+
+#difference
+print("Difference of E and N is", E - N)
+
+#symmetric difference
+print("Symmetric difference of E and N is", E ^ N)
